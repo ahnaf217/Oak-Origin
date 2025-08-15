a7368573bfca1207157b3c95f029e803f0f87880
ahnaf217 <ahnaflabib56@gmail.com>
2025-08-15 16:07:33 +0600

    Shopping cart added

---
+++ demo/Models/Cart.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace demo.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Qty { get; set; }
+
+        public Product Product { get; set; }
+
+        public string UserId { get; set; }
+
+        public Users User
+        {
+            get; set;
+        }
+    }
+}
