536a5933b91e31febaf03875491fff5e50e1a467
Khadiza Khanom <lk221201@gmail.com>
2025-08-31 19:54:43 +0600

    Created Contact Us page.

---
+++ demo/Models/ContactViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace demo.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
