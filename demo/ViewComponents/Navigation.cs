c9548975991a7ee3b3a8a6552a3f000b197a1f39
ahnaf217 <ahnaflabib56@gmail.com>
2025-09-05 22:27:23 +0600

    Dynamic cart count implemented.

---
+++ demo/ViewComponents/Navigation.cs
@@ -0,0 +1,36 @@
+﻿using demo.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using demo.ViewModels;
+using demo.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace demo.ViewComponents
+{
+    [ViewComponent(Name = "Navigation")]
+    public class Navigation : ViewComponent
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<Users> _userManager;
+
+        public Navigation(AppDbContext context, UserManager<Users> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+            var cart = await _context.Carts.Where(x => x.UserId == currentUser.Id).ToListAsync();
+
+            var model = new NavigationViewModel
+            {
+                Cart = cart
+            };
+
+            return View("Index", model);
+        }
+    }
+}
