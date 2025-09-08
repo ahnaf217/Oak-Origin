5eb4795c3bece02f0a35de3e4e7d738a588864cb
ahnaf217 <ahnaflabib56@gmail.com>
2025-09-08 13:35:15 +0600

    Admin logout method created.

--- demo/Program.cs
+++ demo/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options=>
 options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
+
 builder.Services.AddIdentity<Users, IdentityRole>(options =>
 {
     // Password settings
