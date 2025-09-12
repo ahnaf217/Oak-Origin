fab6ef57774ada5b59a04dd2a3b2d28ed1f52557
Nadia-yeasmin <nadiasupti420@gmail.com>
2025-09-12 20:55:11 +0600

    Fix few things

---
+++ demo/Controllers/Blog.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo.Controllers
+{
+    public class Blog : Controller
+    {
+        public IActionResult blog()
+        {
+            return View();
+        }
+    }
+}
